a065071fd4e5401128ea7d6ddbb3a5027eff0b08
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-05-27 05:41:24 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,19 @@
 // declare your functions here...
-function paragraphSelector () { 
+function paragraphSelector () {
 return $('p');
+
+function lastImageSelector(){
+  return$('img:last');
+}
+
+function ninjaBabySelector (){
+  returns $('img:baby-ninja');
+}
+
+function divSelector () {
+  return $('div.pics');
+}
+
+function firstListItem () { 
+return $('ul#pic-list li:first-child');
+}
